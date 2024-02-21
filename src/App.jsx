5bc19e3219ac2a282367e9c53b7b1d1baed4d06f
tankTopTaro <balmoresk3v1n@gmail.com>
2024-02-21 08:05:48 +0800

    added custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import { Routes, Route } from "react-router-dom"
 import Create from './pages/Create'
 import Notes from './pages/Notes'
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    }
+  },
+  typography: {
+    fontFamily: "Kode Mono", 
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  },
+  btn: {
+      hover: {
+          backgroundColor: '#808080'
+      }
+  }
+})
 
 function App() {
 
   return (
-    <Routes>
-      <Route exact path="/" element={<Notes />}/>
-      <Route path="/create" element={<Create />}/>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route exact path="/" element={<Notes />}/>
+        <Route path="/create" element={<Create theme={theme} />}/>
+      </Routes>
+    </ThemeProvider>
   )
 }
 
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,28 +1,32 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import { Typography, Button, Container } from "@mui/material"
 import { KeyboardArrowRight } from "@mui/icons-material"
 
-export default function Create() {
+// eslint-disable-next-line no-unused-vars
+export default function Create({ theme }) {
     return (
-        <Container>
-            <Typography 
-                variant="h6" 
-                color="textSecondary" 
-                component="h2" 
-                gutterBottom
-            >
-                Create a New Note
-            </Typography>
+            <Container>
+                <Typography 
+                    variant="h6" 
+                    color="textSecondary" 
+                    component="h2" 
+                    gutterBottom
+                >
+                    Create a New Note
+                </Typography>
 
-            <Button 
-                onClick={()=>console.log('Clicked')}
-                endIcon={<KeyboardArrowRight />}
-                type="submit" 
-                color="secondary" 
-                variant="contained"
-            >
-                Submit
-            </Button>
-        </Container>
+                <Button 
+                    onClick={()=>console.log('Clicked')}
+                    endIcon={<KeyboardArrowRight />}
+                    type="submit" 
+                    variant="contained"
+                    sx={(theme) => ({
+                        '&:hover' : theme.btn.hover
+                    })}
+                >
+                    Submit
+                </Button>
+            </Container>
     )
 }
